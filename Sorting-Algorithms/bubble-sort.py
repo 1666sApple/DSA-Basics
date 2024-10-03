794ed77f3b15d66ecc6a5ce192af9af7d37a3259
1666sApple <ananno.034@gmail.com>
2024-10-03 14:35:04 +0600

    add bubble-sort

---
+++ Sorting-Algorithms/bubble-sort.py
@@ -0,0 +1,16 @@
+import array
+from re import A
+
+arr = array.array('i', [12, 25, 11, 34, 90, 22])
+
+def bubble_sort(arr):
+    size = len(arr)
+    for j in range(size):
+        for i in range(0, size-1-j):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+
+    return arr
+
+sort_arr = bubble_sort(arr)
+print(sort_arr)
